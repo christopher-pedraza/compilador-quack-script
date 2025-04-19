3322654a548d3258c75384a203f57000b3d00c68
christopher-pedraza <62347713+christopher-pedraza@users.noreply.github.com>
2025-04-19 16:36:22 -0600

    Add some transformer functions and update grammar

--- QuackScript/QuackTransformer.py
+++ QuackScript/QuackTransformer.py
@@ -3,10 +3,16 @@ from lark import Lark, Transformer, v_args
 @v_args(inline=True)
 class QuackTransformer(Transformer):
     def const_decl(self, const, id, colon, type, assign, expression, semicolon):
-        return (id, expression)
+        return ("const_decl", id[0], expression)
     
     def var_decl(self, var, id, colon, type, assign, expression, semicolon):
-        return ("var_decl", id, expression)
+        return ("var_decl", id[0], expression)
 
     def func_decl(self, func, id, lpar, params, rpar, colon, type, assign, block):
         return (id, params, block)
+    
+    def FLOAT(self, value):
+        return float(value)
+    
+    def INT(self, value):
+        return int(value)
