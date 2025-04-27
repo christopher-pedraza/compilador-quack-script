64dbb29699e0fbc20c337d6750ea36fa6f07e3ac
christopher-pedraza <62347713+christopher-pedraza@users.noreply.github.com>
2025-04-27 00:04:19 -0600

    Agregar expresiones y terminos

--- QuackScript/V2/QuackTransformer.py
+++ QuackScript/V2/QuackTransformer.py
@@ -24,3 +24,24 @@ class QuackTransformer(Transformer):
     
     def positive_factor_id(self, id):
         return self.symbol_table.get_variable(name=id, containerName=self.current_container)
+    
+    def factor_cte_num(self, cte_num):
+        return cte_num
+    
+    def positive_cte_num(self, plus, cte_num):
+        return cte_num
+
+    def negative_cte_num(self, minus, cte_num):
+        return -cte_num 
+    
+    def term_mult(self, factor1, mult, factor2):
+        return factor1 * factor2
+    
+    def term_div(self, factor1, div, factor2):
+        return factor1 / factor2
+
+    def exp_plus(self, term1, plus, term2):
+        return term1 + term2
+    
+    def exp_minus(self, term1, minus, term2):
+        return term1 - term2
