186613d5dc2e1f116a56088b853c9939672ae0c1
christopher-pedraza <62347713+christopher-pedraza@users.noreply.github.com>
2025-05-02 09:27:39 -0600

    Route prints to .log files

--- QuackScript/V4/QuackScriptRunner.py
+++ QuackScript/V4/QuackScriptRunner.py
@@ -1,5 +1,6 @@
 import logging
 import argparse
+import sys
 import os
 from lark import Lark, logger, UnexpectedInput
 from QuackTransformer import QuackTransformer
@@ -24,7 +25,7 @@ quack = quackParser.parse
 def parse_program(program):
     try:
         # clear console
-        print("\033[H\033[J", end="")
+        # print("\033[H\033[J", end="")
 
         # Parse the input program
         tree = quack(program)
@@ -54,29 +55,31 @@ if __name__ == "__main__":
     # Toma todos los archivos de la carpeta ./tests, realiza el parseo y guarda
     # el output en un archivo .out por cada uno de los archivos .quack
     # en la carpeta ./output
+        if not os.path.exists("./tests"):
+            os.makedirs("./tests")
+        if not os.path.exists("./output"):
+            os.makedirs("./output")
 
-    if not os.path.exists("./tests"):
-        os.makedirs("./tests")
-    if not os.path.exists("./output"):
-        os.makedirs("./output")
+        for file in os.listdir("./tests"):
+            if file.endswith(".quack"):
+                with open(os.path.join("./output", f"{file[:-6]}.log"), "w", encoding="utf-8") as log_file:
+                    sys.stdout = log_file
+                    with open(os.path.join("./tests", file), 'r', encoding='utf-8') as input_file:
+                        program = input_file.read()
+                        tree, ir, symbol_table = parse_program(program)
+                        # Guarda el output en un archivo .out
+                        with open(os.path.join("./output", file.replace(".quack", ".out")), 'w', encoding='utf-8') as output_file:
+                            output_file.write("Parse Tree:\n")
+                            output_file.write(str(tree))
+                            output_file.write("\n")
+                            output_file.write("IR:\n")
+                            output_file.write(str(ir))
+                            output_file.write("\n\n")
+                            output_file.write("Symbol Table:\n")
+                            output_file.write(symbol_table)
+                            output_file.write("\n")
 
-    for file in os.listdir("./tests"):
-        if file.endswith(".quack"):
-            with open(os.path.join("./tests", file), 'r', encoding='utf-8') as input_file:
-                program = input_file.read()
-                tree, ir, symbol_table = parse_program(program)
-                # Guarda el output en un archivo .out
-                with open(os.path.join("./output", file.replace(".quack", ".out")), 'w', encoding='utf-8') as output_file:
-                    output_file.write("Parse Tree:\n")
-                    output_file.write(str(tree))
-                    output_file.write("\n")
-                    output_file.write("IR:\n")
-                    output_file.write(str(ir))
-                    output_file.write("\n\n")
-                    output_file.write("Symbol Table:\n")
-                    output_file.write(symbol_table)
-                    output_file.write("\n")
-
+                sys.stdout = sys.__stdout__
     # Si se pasa un archivo como argumento, lo parsea y ejecuta
     # el programa, mostrando el resultado en la consola
     # if args.input_file:
