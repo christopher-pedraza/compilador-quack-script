006b8ffcc03187721b6f60bb282c56ca1b6e9f17
christopher-pedraza <62347713+christopher-pedraza@users.noreply.github.com>
2025-05-11 22:14:43 -0600

    Agregar cuadruplos para prints

--- QuackScript/V2/QuackInterpreter.py
+++ QuackScript/V2/QuackInterpreter.py
@@ -248,6 +248,27 @@ class QuackInterpreter:
            
             #############################################################################################################
             elif ir_type == "print":
+                # print_statement = None
+
+                # if isinstance(ir[1], tuple) and ir[1][0] == "quadruple":
+                #     print_statement = ir[1][1]
+                # else:
+                #     print_statement = ir[1]
+
+                # n = 1 if len(print_statement)==1 else len(print_statement)
+                # for i in range(n):
+                #     item = print_statement[i]
+                #     if isinstance(item, tuple) and item[0] == "id":
+                #         var_name = item[1]
+                #         value = self.symbol_table.get_variable(name=var_name, containerName=self.current_container)
+                #         print(value, end=" ")
+                #     elif isinstance(item, tuple) and item[0] == "cte_string":
+                #         value = item[1]
+                #         print(value, end=" ")
+                #     else:
+                #         value = self.evaluate_expression(item)
+                #         print(value, end=" ")
+                # print()
                 print_statement = None
 
                 if isinstance(ir[1], tuple) and ir[1][0] == "quadruple":
@@ -260,15 +281,13 @@ class QuackInterpreter:
                     item = print_statement[i]
                     if isinstance(item, tuple) and item[0] == "id":
                         var_name = item[1]
-                        value = self.symbol_table.get_variable(name=var_name, containerName=self.current_container)
-                        print(value, end=" ")
+                        value = var_name
                     elif isinstance(item, tuple) and item[0] == "cte_string":
                         value = item[1]
-                        print(value, end=" ")
                     else:
-                        value = self.evaluate_expression(item)
-                        print(value, end=" ")
-                print()
+                        value = self._resolve_operand(item)[0]
+                        
+                    self.quack_quadruple.add_quadruple("print", None, None, value)
          
             #############################################################################################################
             elif ir_type == "condition_if":
@@ -337,7 +356,6 @@ class QuackInterpreter:
 
             #############################################################################################################
             elif ir_type == "program":
-                # ("program", id, decls, funcs, body)
                 _, id, decls, funcs, body = ir
                 for decl in decls:
                     self.execute(decl)
