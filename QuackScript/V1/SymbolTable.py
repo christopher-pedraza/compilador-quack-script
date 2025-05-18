5174af8d9d0a5ed224b84399dcf5f63f27d6c157
christopher-pedraza <62347713+christopher-pedraza@users.noreply.github.com>
2025-05-18 12:46:10 -0600

    Refactor QuackScript: Remove V2 implementation and add V1 structure

--- QuackScript/V1/SymbolTable.py
+++ QuackScript/V1/SymbolTable.py
@@ -1,10 +1,14 @@
-from Exceptions import SymbolRedeclarationError, \
-                       ParameterRedeclarationError, \
-                       ParameterMismatchError, \
-                       InvalidParameterIndexError, \
-                       NameNotFoundError, \
-                       CannotModifyConstantError, \
-                       ContainerRedeclarationError
+from Exceptions import (
+    SymbolRedeclarationError,
+    ParameterRedeclarationError,
+    ParameterMismatchError,
+    InvalidParameterIndexError,
+    NameNotFoundError,
+    CannotModifyConstantError,
+    ContainerRedeclarationError,
+    ReservedWordError,
+)
+
 
 class Symbol:
     def __init__(self, name, var_type, value=None, category="var", param_index=None):
@@ -14,17 +18,37 @@ class Symbol:
         self.category = category
         self.param_index = param_index
 
+
 class Container:
     def __init__(self, name, body=None):
         self.name = name
         self.symbols = {}
         self.params = {}
         self.body = body
+        self.reserved_words = [
+            "if",
+            "else",
+            "while",
+            "do",
+            "int",
+            "float",
+            "program",
+            "main",
+            "void",
+            "end",
+            "const",
+            "var",
+            "print",
+            "and",
+            "or",
+        ]
 
     def add_symbol(self, symbol: Symbol) -> None:
         """Add a symbol to the container."""
-        if symbol.name in self.symbols:
+        if symbol.name in self.symbols or symbol.name in self.params:
             raise SymbolRedeclarationError(f"Symbol '{symbol.name}' already exists in '{self.name}'.")
+        if symbol.name in self.reserved_words:
+            raise ReservedWordError(f"Symbol '{symbol.name}' is a reserved word and cannot be used as an identifier.")
         self.symbols[symbol.name] = symbol
 
     def add_param(self, symbol: Symbol) -> None:
@@ -66,16 +90,17 @@ class Container:
     def is_symbol_declared(self, name: str) -> bool:
         """Check if a symbol is declared in the container."""
         return name in self.symbols or name in self.params
-    
+
     def clean_params_values(self) -> None:
         """Reset the values of parameters in the container."""
         for param in self.params.values():
             param.value = None
 
+
 class SymbolTable:
     def __init__(self):
         self.containers = {}
-        self.add_container(Container("global", None))
+        self.global_container_name = "global"
 
     def get_container(self, name: str) -> Container:
         """Get a container by name."""
@@ -100,26 +125,30 @@ class SymbolTable:
         if container.is_symbol_declared(name):
             return container.get_symbol(name)
         else:
-            container = self.containers["global"]
+            container = self.containers[self.global_container_name]
             if container.is_symbol_declared(name):
                 return container.get_symbol(name)
             else:
                 raise NameNotFoundError(f"Symbol '{name}' not found in '{containerName}' or global container.")
-            
+
     def __update_symbol(self, name: str, value, containerName: str) -> None:
         """Update the value of a symbol in the specified container."""
         container = self.get_container(containerName)
         if container.is_symbol_declared(name):
             container.update_symbol(name, value)
         else:
-            container = self.containers["global"]
+            container = self.containers[self.global_container_name]
             if container.is_symbol_declared(name):
                 container.update_symbol(name, value)
             else:
                 raise NameNotFoundError(f"Symbol '{name}' not found in '{containerName}' or global container.")
-            
-    def add_variable(self, name: str, var_type: str, value=None, category="var", containerName: str = "global", param_index: int = None) -> None:
+
+    def add_variable(
+        self, name: str, var_type: str, value=None, category="var", containerName: str = None, param_index: int = None
+    ) -> None:
         """Add a variable to the specified container."""
+        if containerName is None:
+            containerName = self.global_container_name
         variable = Symbol(name=name, var_type=var_type, value=value, category=category, param_index=param_index)
         self.__add_symbol(variable, containerName)
 
@@ -131,12 +160,16 @@ class SymbolTable:
 
     def get_variable(self, name: str, containerName: str) -> Symbol:
         """Get a variable from the specified container."""
+        return self.__get_symbol(name, containerName)
+
+    def get_variable_value(self, name: str, containerName: str) -> Symbol:
+        """Get a variable from the specified container."""
         return self.__get_symbol(name, containerName).value
-    
+
     def get_variable_type(self, name: str, containerName: str) -> str:
         """Get the type of a variable from the specified container."""
         return self.__get_symbol(name, containerName).var_type
-    
+
     def update_variable(self, name: str, value, containerName: str) -> None:
         """Update the value of a variable in the specified container."""
         self.__update_symbol(name, value, containerName)
@@ -144,11 +177,11 @@ class SymbolTable:
     def add_function(self, name: str, params: list, body) -> None:
         """Add a function as a container"""
         self.add_container(Container(name, body))
-        
+
         for i, param in enumerate(params):
             param_name, param_type = param
             self.add_parameter(name=param_name, var_type=param_type, containerName=name, param_index=i)
-        
+
     def update_params_values(self, containerName: str, values: list) -> None:
         """Set the values of parameters in the specified container."""
         container = self.get_container(containerName)
@@ -158,7 +191,7 @@ class SymbolTable:
         """Reset the values of parameters in the specified container."""
         container = self.get_container(containerName)
         container.clean_params_values()
-        
+
     def display(self) -> None:
         """Display the contents of the symbol table."""
         for container_name, container in self.containers.items():
@@ -180,5 +213,16 @@ class SymbolTable:
                 result += f"  {param_name}: {param.var_type}, {param.value}, {param.category}, {param.param_index}\n"
             result += "  Symbols:\n"
             for symbol_name, symbol in container.symbols.items():
-                result += f"  {symbol_name}: {symbol.var_type}, {symbol.value}, {symbol.category}, {symbol.param_index}\n"
+                result += (
+                    f"  {symbol_name}: {symbol.var_type}, {symbol.value}, {symbol.category}, {symbol.param_index}\n"
+                )
         return result
+
+    def create_global_container(self, id):
+        """Create a global container with the given id."""
+        self.add_container(Container(id, None))
+        self.global_container_name = id
+
+    def __str__(self):
+        """String representation of the symbol table."""
+        return self.get_str_representation()
